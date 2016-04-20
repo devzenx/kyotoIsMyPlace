63a1094a558d8f12866b6d7e76fad55f82dbec3b
stephane <contact@depoilly.com>
2016-04-21 00:52:50 +0200

    Initial Commit

---
+++ KyotoIsMyPlace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KyotoIsMyPlace
+//
+//  Created by Stéphane DEPOILLY on 21/04/2016.
+//  Copyright © 2016 Stéphane DEPOILLY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
