9b1219f940b2ef4852de3e5cb0b16601a178ce8d
stephane <contact@depoilly.com>
2016-04-22 22:02:40 +0200

    All screen fit on iPad and iPhone. Exercise done.

--- KyotoIsMyPlace/KyotoInPhotos.swift
+++ KyotoIsMyPlace/KyotoInPhotos.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class KyotoInPhotos: UIViewController {
 
+    var slideshowArray: [String] = ["slide1","slide2","slide3","slide4","slide5"]
+    
+    @IBOutlet weak var imgSlideshow: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,5 +24,23 @@ class KyotoInPhotos: UIViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
 
-
+    @IBAction func loadSlide1(sender: AnyObject) {
+        imgSlideshow.image = UIImage (named: "\(slideshowArray[0]).jpg")
+    }
+    
+    @IBAction func loadSlide2(sender: AnyObject) {
+        imgSlideshow.image = UIImage (named: "\(slideshowArray[1]).jpg")
+    }
+    
+    @IBAction func loadSlide3(sender: AnyObject) {
+        imgSlideshow.image = UIImage (named: "\(slideshowArray[2]).jpg")
+    }
+    
+    @IBAction func loadSlide4(sender: AnyObject) {
+        imgSlideshow.image = UIImage (named: "\(slideshowArray[3]).jpg")
+    }
+    
+    @IBAction func loadSlide5(sender: AnyObject) {
+        imgSlideshow.image = UIImage (named: "\(slideshowArray[4]).jpg")
+    }
 }
--- KyotoIsMyPlace/ViewController.swift
+++ KyotoIsMyPlace/ViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController {    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,10 +18,6 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func loadPhotos(sender: AnyObject) {
-        print("photos")
     }
 
 }
